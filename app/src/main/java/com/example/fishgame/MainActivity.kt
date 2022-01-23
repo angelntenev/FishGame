a23ed4d9c635916863dca60580c7ba0a66a5e083
Angel Tenev <40444294@live.napier.ac.uk>
2022-01-23 15:43:50 +0000

    Initial commit

---
+++ app/src/main/java/com/example/fishgame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fishgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
