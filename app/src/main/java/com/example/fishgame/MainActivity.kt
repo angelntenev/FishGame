992e004ec0917a643d6feb630feb726416897160
Angel Tenev <40444294@live.napier.ac.uk>
2022-01-23 23:43:37 +0000

    Basic view created. App is in landscape.

--- app/src/main/java/com/example/fishgame/MainActivity.kt
+++ app/src/main/java/com/example/fishgame/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.fishgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
     }
 }
